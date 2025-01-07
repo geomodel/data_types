1a2e5f2cf24583c234ed34f37e91fb76b0a1b7cf
raa <alexander.thequeen@gmail.com>
2025-01-07 22:57:14 +0300

    saving

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ pub type Discrete = i16;
 
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub struct IJK {
-    pub i: usize,
-    pub j: usize,
-    pub k: usize,
+    pub i: Discrete,
+    pub j: Discrete,
+    pub k: Discrete,
 }
 
 impl std::ops::Add for IJK {
@@ -21,15 +21,22 @@ impl std::ops::Add for IJK {
         }
     }
 }
+impl std::ops::Neg for IJK {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self {
+            i: -self.i,
+            j: -self.j,
+            k: -self.k,
+        }
+    }
+}
 impl std::ops::Sub for IJK {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
-        Self {
-            i: self.i - other.i,
-            j: self.j - other.j,
-            k: self.k - other.k,
-        }
+        self + -other
     }
 }
 
@@ -47,6 +54,12 @@ mod ijk_testing {
         let b = IJK{ i: 2, j: 3, k: 4 };
         let r = IJK{ i: 3, j: 5, k: 7 };
         assert!( r == (a+b) );
+    }
+    #[test]
+    fn check_neg() {
+        let a = IJK{ i: 1, j: 2, k: 3 };
+        let r = IJK{ i: -1, j: -2, k: -3 };
+        assert!( r == -a );
     }
     #[test]
     fn check_sub() {
